5034fa4931ee70bbc1eae608b46080cfcbec0318
MiyanagaShimon <sm43001999@gmail.com>
2018-09-14 10:17:12 +0900

    menu_bar 180912

--- web/libs/menu_bar/menu_bar.js
+++ web/libs/menu_bar/menu_bar.js
@@ -1,2 +1,18 @@
-﻿var body = document.getElementByTagName('body');
-body.appendChild(div);
+﻿//メニューバーの大枠----------------------------------------------
+var menuBarWrap= document.createElement("div");
+menuBarWrap.setAttribute('class','menuBarWrap_m');
+
+//ロゴのリンクを作成---------------------------------------------
+var logo = document.createElement('a');
+logo.setAttribute('class', 'logo_m');
+logo.setAttribute('href', '');
+var logoImg = document.createElement('img');
+logoImg.setAttribute('class', 'logoImg_m');
+logoImg.setAttribute('src', '../libs/menu_bar/logo.png');
+logo.appendChild(logoImg);
+
+
+//
+menuBarWrap.appendChild(logo);
+
+document.body.appendChild(menuBarWrap);
