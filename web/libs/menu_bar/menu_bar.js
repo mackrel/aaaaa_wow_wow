f37d61c88a3f7f76de109d7f371b804af1fecca7
MiyanagaShimon <sm43001999@gmail.com>
2018-09-14 14:59:40 +0900

    manubar style

--- web/libs/menu_bar/menu_bar.js
+++ web/libs/menu_bar/menu_bar.js
@@ -2,7 +2,7 @@
 var menuBarWrap= document.createElement("div");
 menuBarWrap.setAttribute('class','menuBarWrap_m');
 
-//ロゴのリンクを作成---------------------------------------------
+//ロゴのリンクを作成----------------------------------------------
 var logo = document.createElement('a');
 logo.setAttribute('class', 'logo_m');
 logo.setAttribute('href', '');
@@ -11,8 +11,21 @@ logoImg.setAttribute('class', 'logoImg_m');
 logoImg.setAttribute('src', '../libs/menu_bar/logo.png');
 logo.appendChild(logoImg);
 
+//ログアウトボタン
+var logout = document.createElement('a');
+logout.setAttribute('class', 'logout_m');
+logout.setAttribute('href', '');
+var logoutTxt = document.createElement('p');
+logoutTxt.setAttribute('class', 'logoutTxt_m');
+logoutTxt.innerText = "ログアウト";
+logout.appendChild(logoutTxt);
+var logoutImg = document.createElement('img');
+logoutImg.setAttribute('class', 'logoutImg_m');
+logoutImg.setAttribute('src', '../libs/menu_bar/exit.png');
+logout.appendChild(logoutImg);
 
 //
 menuBarWrap.appendChild(logo);
+menuBarWrap.appendChild(logout);
 
 document.body.appendChild(menuBarWrap);
