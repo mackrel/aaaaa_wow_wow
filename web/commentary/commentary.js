b0ac32273464d6b96e164595aede1ff444a424e0
MiyanagaShimon <sm43001999@gmail.com>
2018-09-18 10:13:40 +0900

    commentary

---
+++ web/commentary/commentary.js
@@ -0,0 +1,10 @@
+﻿$(function(){
+	$('.cmtBtn').on('click',function(){
+		$('.detail').attr('style','display: none')
+		$(this).next().attr('style','display: block');
+	})
+	
+	$('.back').on('click',function(){
+		$(this).parent().attr('style','display: none');
+	})
+});
